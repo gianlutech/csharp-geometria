55d061373d87ae808346aaab116c171d3f5dbc33
gianlutech <gianluca.ingrao95@gmail.com>
2022-05-13 09:09:28 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+
+
+/*
+Nel progetto csharp-geometria creare le seguenti classi
+1.  Creare una classe Rettangolo con due attributi interi: baseRettangolo e altezzaRettangolo.
+    Aggiungere un opportuno costruttore con parametri. 
+    Aggiungere due metodi: calcolaArea e calcolaPerimetro che calcolano e 
+    restituiscono, rispettivamente, l’area e il perimetro del rettangolo.
+
+2.  Poi, nel vostro programma principale (Program.cs) chiedete all’utente 
+    di inserire, da console, i valori di base e di altezza con cui istanziare un nuovo Rettangolo.
+    Dopo averlo istanziato, stampate a video il perimetro e l’area.
+
+3.  Provate ad istanziare tanti rettangoli con nome diverso e attributi diversi 
+    (baseRettangolo e altezzaRettangolo) e provate a stampare le loro proprietà 
+    ad esempio con un metodo StampaRettangolo che vi stampi il vostro rettangolo ad 
+    esempio cosi:
+
+        ——  Rettangolo1  ——
+        base: 20 cm
+        altezza: 10 cm
+        Perimetro: 60 cm
+        Area: 200 cm2
+
+BONUS
+    Aggiungere alla classe Rettangolo un metodo “disegna” che disegna in console il rettangolo 
+    con le sue dimensioni, ossia tanti “—” (due trattini) orizzontali quanto è grande la sua 
+    base e tanti ‘|’ verticali quanto e grande la sua altezza.
+ * */
+namespace csharp_geometria // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double baseRect, altezzaRect;
+            LeggiNumeri.ReadNum("Inserisci base di un rettangolo : ", out baseRect);
+            LeggiNumeri.ReadNum("Inserisci altezza del rettangolo: ", out altezzaRect);
+
+            Rettangolo r1 = new Rettangolo(baseRect, altezzaRect);
+
+            Console.WriteLine("Area: {0}", r1.CalcolaArea());
+            Console.WriteLine("Perimetro: {0}", r1.CalcolaPerimetro());
+
+
+            Rettangolo r2 = new Rettangolo(10, 20, "Rett 1");
+            Rettangolo r3 = new Rettangolo(110, 320, "Rett 2");
+            Rettangolo r4 = new Rettangolo(210, 540, "Rett 3");
+
+            r2.StampaRettangolo();
+            r3.StampaRettangolo();
+            r4.StampaRettangolo();
+
+
+            r2.Disegna();
+        }
+    }
+}
---
+++ Rettangolo.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_geometria
+{
+    internal class Rettangolo
+    {
+        private readonly double baseRettangolo;
+        private readonly double altezzaRettangolo;
+
+        private readonly string nomeRettangolo; //lo chiede nella parte finale dell'esercizio
+                                                //quando dice di creare tanti rettangoli con nomi diversi...
+
+        //Costruttore senza parametri, inizializza tutto a 0
+        //Notare che un costruttore non ha mai un valore di ritorno!!!
+        public Rettangolo()
+        {
+            baseRettangolo = 0;
+            altezzaRettangolo = 0;
+            nomeRettangolo = "Rettangolo";
+        }
+
+        //Costruttore con due parametri, per creare rettangolo con base e altezza dati
+        public Rettangolo(double baseR, double altezzaR) //non posso usare "base" poichè è parola chiave di C#
+        {
+            baseRettangolo = baseR;
+            altezzaRettangolo = altezzaR;
+            nomeRettangolo = "Rettangolo";
+        }
+
+        //Costruttore con tre parametri, per creare rettangolo con base e altezza dati
+        public Rettangolo(double baseRettangolo, double altezzaR, string nomeR)
+        {
+            this.baseRettangolo = baseRettangolo;
+            altezzaRettangolo = altezzaR;
+            nomeRettangolo = nomeR;
+        }
+
+        public double CalcolaArea()
+        {
+            //area del rettandolo: base * altezza
+            return baseRettangolo * altezzaRettangolo;
+        }
+
+        public double CalcolaPerimetro()
+        {
+            //Perimetrodel rettandolo: 2*base + 2*altezza
+            return baseRettangolo * 2 + altezzaRettangolo * 2;
+        }
+
+        public void StampaRettangolo()
+        {
+            Console.WriteLine("—— {0} ——", nomeRettangolo);
+            Console.WriteLine("base: {0} cm", baseRettangolo);
+            Console.WriteLine("altezza: {0} cm", altezzaRettangolo);
+            Console.WriteLine("Perimetro: {0} cm", CalcolaPerimetro());
+            Console.WriteLine("Area: {0} cm", CalcolaArea());
+            Console.WriteLine();
+        }
+
+        public void Disegna()
+        {
+            Console.Write("+");
+            for (int i = 1; i < (baseRettangolo-1); ++i)
+                Console.Write("-");
+            Console.WriteLine("+");
+
+            for (int i = 1; i < (altezzaRettangolo - 1); ++i)
+                Console.WriteLine("|{0}|", new String(' ', (int)(baseRettangolo-2)));
+
+            Console.Write("+");
+            for (int i = 1; i < (baseRettangolo - 1); ++i)
+                Console.Write("-");
+            Console.WriteLine("+");
+        }
+    }
+}
